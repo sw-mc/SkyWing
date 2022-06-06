07d59201594e7b19abd5638d3f16ec795f1353b1
Armen Deroian <cosmic5173@gmail.com>
2022-06-05 23:13:51 -0400

    Beginning of startup

--- Program.cs
+++ Program.cs
@@ -1,8 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-class Program {
+using SkyWing.Logger;
+using SkyWing.SkyWing;
+using SkyWing.SkyWing.Utils;
+
+public class Program {
+
+    private static void Main(string[] args) {
+
+        var pid = FileSystem.CreateLockFile("server.lock");
+        if (pid != null) {
+            CriticalError("Another " + VersionInfo.NAME + " instance (PID)" + pid + " is already using this folder.");
+            CriticalError("Please stop the other server first before running a new one.\n\n");
+            
+            Info("Press any key to exit...");
+            Console.ReadKey();
+            Environment.Exit(1);
+        }
+        
+        //TODO: Timezone setting field, preferably system default timezone.
+        var logger = new MainLogger("server.log", "Server", TimeZoneInfo.Local, false);
+        GlobalLogger.Logger = logger;
+        
+        
+    }
+
+    private static void CriticalError(string message) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[ERROR] {message}");
+        Console.ResetColor();
+    }
     
-    static void Main(string[] args) {
-        Console.WriteLine("Hello World!");
+    private static void Info(string message) {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine($"[INFO] {message}");
+        Console.ResetColor();
     }
 }
---
+++ VersionInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace SkyWing.SkyWing; 
+
+public sealed class VersionInfo {
+    
+    public const string NAME = "SkyWing";
+    public const string VERSION = "1.0.0";
+    public const bool IS_DEVELOPMENT_BUILD = true;
+
+}
