29d263fda554acf4405511ae2ec3cd4df712955b
Armen Deroian <cosmic5173@gmail.com>
2022-06-13 22:49:12 -0400

    Implement language

---
+++ language/KnownTranslation.cs
@@ -0,0 +1,13 @@
+﻿namespace SkyWing.SkyWing.language; 
+
+public class KnownTranslationFactory {
+    
+    //TODO: Create methods to translate messages as we get them.
+    
+}
+
+public sealed class KnownTranslationKeys {
+    
+    //TODO: List translation ids as we get them.
+    
+} 
