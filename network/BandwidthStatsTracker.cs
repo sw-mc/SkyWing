d78a4683341bb1fdafe239e8848584b8f40acad1
Armen Deroian <cosmic5173@gmail.com>
2022-06-13 21:36:47 -0400

    Start Network

---
+++ network/BandwidthStatsTracker.cs
@@ -0,0 +1,59 @@
+﻿namespace SkyWing.SkyWing.Network;
+
+public class BandwidthStatsTracker {
+
+    private long[] history;
+    private int nextIndex;
+    private long bytesSinceLast;
+    public long TotalBytes { get; private set; }
+
+    public BandwidthStatsTracker(int historySize) {
+        history = new long[historySize];
+        Array.Fill(history, 0, 0, historySize);
+    }
+
+    public void Add(long bytes) {
+        TotalBytes += bytes;
+        bytesSinceLast += bytes;
+    }
+
+    public void RotateHistory() {
+        history[nextIndex] = bytesSinceLast;
+        bytesSinceLast = 0;
+        nextIndex = (nextIndex + 1) % history.Length;
+    }
+
+    public long AverageBytes => history.Sum() / history.Length;
+
+    public void ResetHistory() {
+        var historySize = history.Length;
+        history = new long[historySize];
+        Array.Fill(history, 0, 0, historySize);
+    }
+}
+
+public class BidirectionalBandwidthStatsTracker {
+
+    public BandwidthStatsTracker Send { get; }
+    public BandwidthStatsTracker Receive { get; }
+
+    public BidirectionalBandwidthStatsTracker(int historySize) {
+        Send = new BandwidthStatsTracker(historySize);
+        Receive = new BandwidthStatsTracker(historySize);
+    }
+
+    public void Add(long sendBytes, long recvBytes) {
+        Send.Add(sendBytes);
+        Receive.Add(recvBytes);
+    }
+
+    public void RotateHistory() {
+        Send.RotateHistory();
+        Receive.RotateHistory();
+    }
+
+    public void ResetHistory() {
+        Send.ResetHistory();
+        Receive.ResetHistory();
+    }
+}
---
+++ network/mcpe/Packet.cs
@@ -0,0 +1,24 @@
+﻿using BedrockProtocol.Packet;
+
+namespace SkyWing.SkyWing.Network.Mcpe;
+
+public interface PacketSender {
+
+    public void Send(byte[] payload, bool immediate);
+
+    public void Close(string reason = "unknown reason");
+    
+}
+
+public interface PacketBroadcaster {
+
+    public void BroadcastPackets(NetworkSession[] recipients, ClientboundPacket[] packets);
+    
+}
+
+public class PacketHandlingException : Exception{
+
+    public PacketHandlingException(string message) : base(message) {
+    }
+    
+}
